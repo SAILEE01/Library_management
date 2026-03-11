1e049fbae2f729654fd63a146c5d10d8bb998bd1
SAILEE01 <126793864+SAILEE01@users.noreply.github.com>
2026-03-11 16:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,7 @@ def check():
     result = proxy.check_book(book_id)
     return render_template('index.html', result=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
